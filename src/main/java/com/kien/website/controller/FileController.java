014eb9a60165f97cd2793e0367726fe8214f680c
Kien Tran <kien868788@gmail.com>
2019-04-09 10:02:10 +0700

    check

--- src/main/java/com/kien/website/controller/FileController.java
+++ src/main/java/com/kien/website/controller/FileController.java
@@ -1,6 +1,8 @@
 package com.kien.website.controller;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -14,7 +16,9 @@ import java.io.*;
 
 @Controller
 @RequestMapping("/files")
-public class FileController{
+public class FileController {
+
+    Logger logger = LoggerFactory.getLogger(FileController.class);
 
     @Autowired
     public HttpServletRequest request;
@@ -24,30 +28,34 @@ public class FileController{
     @PostMapping("/upload")
     @CrossOrigin
     @PreAuthorize("hasRole('ADMIN')")
-    public @ResponseBody String fileUpload(@RequestParam("file") MultipartFile file)
-            throws IOException {
-        if (!file.getOriginalFilename().isEmpty()) {
-            BufferedOutputStream outputStream =
-                    new BufferedOutputStream(
-                            new FileOutputStream(
-                                    new File(path + file.getOriginalFilename())
-                            )
-                    );
-            outputStream.write(file.getBytes());
-            outputStream.flush();
-            outputStream.close();
+    public @ResponseBody
+    String fileUpload(@RequestParam("file") MultipartFile file) {
+        try {
+            if (!file.getOriginalFilename().isEmpty()) {
+                BufferedOutputStream outputStream =
+                        new BufferedOutputStream(
+                                new FileOutputStream(
+                                        new File(path + file.getOriginalFilename())
+                                )
+                        );
+                outputStream.write(file.getBytes());
+                outputStream.flush();
+                outputStream.close();
+            }
+        } catch (Exception e) {
+            logger.error("Can't upload image",e);
         }
-        return request.getRequestURL().toString().replace("upload",file.getOriginalFilename());
+        return request.getRequestURL().toString().replace("upload", file.getOriginalFilename());
     }
 
     @GetMapping("/{url}")
     @ResponseBody
     public void getFile(@PathVariable("url") String url, HttpServletResponse response) throws Exception {
-            InputStream inputStream = new FileInputStream(
-                    new File(path + url)
-            );
-            IOUtils.copy(inputStream,response.getOutputStream());
-            response.flushBuffer();
+        InputStream inputStream = new FileInputStream(
+                new File(path + url)
+        );
+        IOUtils.copy(inputStream, response.getOutputStream());
+        response.flushBuffer();
     }
 
 }
